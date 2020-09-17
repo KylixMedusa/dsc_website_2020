aead7d47ebd7314b73a36afe4e91033ace19b97f
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-09-17 19:16:41 +0530

    Added navbar component, dynamic routing, styling

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+
+export const routes: RouteInfo[]=[
+  { path: '/home', title: 'Home',  icon: 'fa-home', class: '' },
+  { path: '/events', title: 'Events',  icon:'fa-calendar', class: '' },
+  { path: '/projects', title: 'Projects', icon:'fa-sticky-note', class:''},
+  {path:'/team',title:'Team',icon:'fa-users',class:''}
+];
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +21,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  menuItems:any[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.menuItems = routes.filter(menuItem => menuItem);
   }
 
 }
