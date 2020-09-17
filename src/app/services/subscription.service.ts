a5b6f761a4523e466bcb3ffbcc8e7a013baee130
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-09-17 19:15:26 +0530

    Added subscription service

---
+++ src/app/services/subscription.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubscriptionService {
+
+
+  constructor() {
+   }
+
+}
